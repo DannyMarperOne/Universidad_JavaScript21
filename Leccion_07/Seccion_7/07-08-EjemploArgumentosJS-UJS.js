7f538ec4778eb39d34583060619d40815f8c88d9
DannyMarperOne <81833593+DannyMarperOne@users.noreply.github.com>
2021-08-03 15:17:34 -0500

    jejej

--- Leccion_07/Seccion_7/07-08-EjemploArgumentosJS-UJS.js
+++ Leccion_07/Seccion_7/07-08-EjemploArgumentosJS-UJS.js
@@ -1,9 +1,9 @@
 let resultado = sumarTodo(5, 4, 13, 10, 9, 10, 11, 3);
-console.log( resultado );
+console.log(resultado);
 
-function sumarTodo(){
+function sumarTodo() {
     let suma = 0;
-    for(let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
         suma += arguments[i]; // suma = suma + arguments[i]
     }
     return suma;
